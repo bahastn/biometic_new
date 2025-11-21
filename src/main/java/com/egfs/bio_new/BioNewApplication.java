a7f98f56d0133da6c861fb7969441a9de4145468
Bahast <bahastn@yahoo.com>
2025-11-21 20:06:20 +0300

    first commit

---
+++ src/main/java/com/egfs/bio_new/BioNewApplication.java
@@ -0,0 +1,13 @@
+package com.egfs.bio_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egfs/bio_new/BioNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egfs.bio_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
